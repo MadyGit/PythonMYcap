43dd165fe3e9b3e6806c91562b6e2dcaabaac86b
Manthra H <47036388+MadyGit@users.noreply.github.com>
2021-03-21 14:07:08 +0530

    PositiveNumbers

---
+++ PositiveNumbers.py
@@ -0,0 +1,26 @@
+print("output 1:");
+# list 1 of numbers 
+list1 = [12,-7,5,64,-14] 
+
+# iterating each number in list1 
+for num in list1: 
+      
+    # checking condition 
+    if num > 0: 
+       print(num, end = " ")
+
+# list 2 of numbers 
+print("\noutput 2:");
+list2 = [12,14,-95,3] 
+
+# iterating each number in list2 
+for num in list2: 
+      
+    # checking condition 
+    if num > 0: 
+       print(num, end = " ")
+        
+#output 1:
+#12 5 64 
+#output 2:
+#12 14 3
